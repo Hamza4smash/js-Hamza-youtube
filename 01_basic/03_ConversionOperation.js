b1e983063d089cfc674eb53ecda9d2887329f5e4
Hamza4smash <96951409+Hamza4smash@users.noreply.github.com>
2026-08-30 04:00:03 +0000

    Conversion  of  operation

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -22,5 +22,25 @@ let isLoggedIn=""
 
 let anyNumber=3
 let stringNumber=String(anyNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber); //string
+// console.log(stringNumber)
+// console.log(typeof stringNumber); //
+
+
+// **********************operations************************
+let value=7
+let negValue= -value
+// console.log(negValue);
+
+
+let str1="Hello"
+let str2="Hamza"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+
+
