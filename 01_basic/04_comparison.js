5bd4fbc937f9f7d56e4d0b6935551bb78ac6483c
Hamza4smash <96951409+Hamza4smash@users.noreply.github.com>
2026-08-30 12:51:12 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+//avoid these conversion as they are very confusing and can lead to bugs in your code   
+// ===
+
+console.log("2" === 2);
