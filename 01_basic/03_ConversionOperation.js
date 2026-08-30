f443b63832fcbc1c0c16cb3e8eae6488f3d104f8
Hamza4smash <96951409+Hamza4smash@users.noreply.github.com>
2026-08-30 03:22:37 +0000

    Conversion Of Datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+//let age = "25";
+
+//console.log(typeof age); // number
+//console.log(typeof (age));
+
+//let valueInNumber = Number(age)
+
+//console.log(typeof valueInNumber); // number
+
+//"33"= 33
+//"33abc"= NaN
+//"true"=1 and "false"=0
+
+let isLoggedIn=""
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1= true;
+//""=false
+//"hamza=true"
+
+let anyNumber=3
+let stringNumber=String(anyNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber); //string
